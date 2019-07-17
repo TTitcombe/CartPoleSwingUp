fc611ddfa86f50f6c19bd15228882ba925348c38
Thomas Titcombe <thomas.titcombe@tessella.com>
2019-07-17 08:16:50 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CartPoleSwingUp',
+      version='1.0',
+      description='CartPoleSwingUp custom openai gym environment',
+      author='Tom Titcombe',
+      author_email='t.j.titcombe@gmail.com',
+      packages=['gym']
+      )
